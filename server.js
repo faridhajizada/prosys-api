f7efb9dba934a376566bf0f70a5eb8779767194c
Hajizada Farid <faridmh@code.edu.az>
2024-09-12 00:45:22 +0400

    feat: add redux and student api

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const studentRoutes = require('./routes/studentRoutes');
-const lessonRoutes = require('./routes/lessonRoutes');
-const examRoutes = require('./routes/examRoutes');
-const { swaggerUi, specs } = require('./swagger');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const studentRoutes = require("./routes/studentRoutes");
+const lessonRoutes = require("./routes/lessonRoutes");
+const examRoutes = require("./routes/examRoutes");
+const { swaggerUi, specs } = require("./swagger");
 
 const app = express();
 const PORT = 3009;
+
+app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
-app.use('/api/students', studentRoutes);
-app.use('/api/lessons', lessonRoutes);
-app.use('/api/exams', examRoutes);
+app.use("/api/students", studentRoutes);
+app.use("/api/lessons", lessonRoutes);
+app.use("/api/exams", examRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-  console.log(`Swagger documentation available at http://localhost:${PORT}/api-docs`);
+  console.log(
+    `Swagger documentation available at http://localhost:${PORT}/api-docs`
+  );
 });
